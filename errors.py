654eebaebea59e7d728a36cd15c19d7c152a0d90
hnduong99 <duong.hoangnam.dhn@gmail.com>
2021-07-12 12:27:04 +1000

    Create errors.py
    Create different error messages

---
+++ errors.py
@@ -0,0 +1,8 @@
+## ERROR CODE
+#
+# @author: Vincent
+# @date: 12 May 2021
+
+MONITOR_4ERR_LOGIN_ERROR = 'Login API Error'
+MONITOR_4ERR_LOGOUT = 'Logout API Error'
+MONITOR_4ERR_REGISTER = 'Register API Error'
